366ae2f5376a7d97b07a679f62fadad78ec14837
Pyae Phyo Zaw <pyaephyoezaw.2019@gmail.com>
2026-03-24 09:30:19 +0630

    test: add H2 for CI environment independence

--- backend/src/test/java/com/example/todo/TodoRepositoryTests.java
+++ backend/src/test/java/com/example/todo/TodoRepositoryTests.java
@@ -6,11 +6,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 
 import com.example.todo.model.Todo;
 import com.example.todo.repository.TodoRepository;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class TodoRepositoryTests {
 
     @Autowired
